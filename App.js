d78bf71d7f0497632c0692ec665e9991f07c24e2
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-11-11 20:24:03 +0100

    style the home with input and button and show massage area

--- App.js
+++ App.js
@@ -7,27 +7,100 @@
  */
 
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button, Image } from "react-native";
+import {
+  TextInput,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  Linking,
+  ImageBackground
+} from "react-native";
+import catImg from "./src/assets/cat.png";
 
 export default class App extends Component {
   state = {
-    gre: null
+    text: "",
+    masseges: []
   };
-  onPressLearnMore = () => {
-    this.setState({ gre: "Hallo Ghassanooooo IM React Native" });
+  onPressSend = () => {
+    console.log("the button clicked");
+    const cloneState = {
+      ...this.state
+    };
+    cloneState.masseges.push(this.state.text);
+    this.setState({ masseges: cloneState.masseges, text: "" });
   };
 
   render() {
     return (
-      <View style={styles.container}>
-        <Image
-          style={{ width: 300, height: 250 }}
-          source={{
-            uri:
-              "https://static.next-episode.net/tv-shows-images/huge/one-piece-jp.jpg"
+      <ImageBackground
+        source={catImg}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <View
+          style={{
+            textAlign: "center",
+            alignItems: "center",
+            padding: 10,
+            backgroundColor: "#850C85"
           }}
-        />
-      </View>
+        >
+          <Text
+            style={{ color: "#fff", fontSize: 40, justifyContent: "center" }}
+          >
+            Bazon
+          </Text>
+        </View>
+        <View
+          style={{
+            width: "100%",
+            backgroundColor: "rgba(0,0,0,0.4)",
+            height: "80%"
+          }}
+        >
+          {this.state.masseges &&
+            this.state.masseges.map((massege, id) => (
+              <Text style={{ color: "#fff", fontSize: 20 }} key={id}>
+                {massege}
+              </Text>
+            ))}
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            backgroundColor: "red"
+          }}
+        >
+          <View
+            style={{
+              width: "20%",
+
+              justifyContent: "center"
+            }}
+          >
+            <Button
+              onPress={this.onPressSend}
+              title="Login"
+              color="red"
+              accessibilityLabel="Learn more about this purple"
+            />
+          </View>
+          <TextInput
+            style={{
+              backgroundColor: "#fff",
+              marginBottom: 10,
+              width: "80%"
+            }}
+            placeholder="feed message..."
+            onChangeText={text => this.setState({ text })}
+            textContentType={"password"}
+            defaultValue={this.state.text}
+          />
+        </View>
+      </ImageBackground>
     );
   }
 }
@@ -36,18 +109,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
-    color: "red"
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+    alignItems: "center"
   }
 });
